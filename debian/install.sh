5cb5e0df476e4be6d47680d3285fe928de2e3e61
mamemomonga <mamemomonga@gmail.com>
2016-09-05 15:24:53 +0900

    install.sh 更新

--- debian/install.sh
+++ debian/install.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
+set -xe
 
 sudo sh << 'END_OF_SUDO'
 set -xe
 
-cat > /etc/apt/sources.list.d/backports.list' << 'EOS'
+cat > /etc/apt/sources.list.d/backports.list << 'EOS'
 deb http://ftp.jp.debian.org/debian jessie-backports main
 EOS
 
@@ -11,7 +12,7 @@ aptitude update
 aptitude install -y apt-transport-https ca-certificates
 apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
 
-sh -c 'cat > /etc/apt/sources.list.d/docker.list' << 'EOS'
+cat > /etc/apt/sources.list.d/docker.list << 'EOS'
 deb https://apt.dockerproject.org/repo debian-jessie main
 EOS
 
@@ -34,6 +35,7 @@ for i in compose machine; do
 done
 
 END_OF_SUDO
+
 
 if [ -z $(getent group docker) ]; then
   sudo groupadd docker
